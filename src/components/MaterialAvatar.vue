a76a5ecc48abba937a90cb30d35d0f58e4980067
gaofeng9 <gaofeng9@kingsoft.com>
2024-08-30 10:17:13 +0800

    【项目初始化】

---
+++ src/components/MaterialAvatar.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  image: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String,
+    default: "xxl",
+  },
+  borderRadius: {
+    type: String,
+    default: "",
+  },
+});
+function getClasses(size, borderRadius) {
+  let sizeValue = size && `avatar-${size}`;
+  let borderRadiusValue = borderRadius && `border-radius-${borderRadius}`;
+
+  return `${sizeValue} ${borderRadiusValue}`;
+}
+</script>
+<template>
+  <img
+    class="avatar"
+    :src="image"
+    :class="getClasses(size, borderRadius)"
+  />
+</template>
