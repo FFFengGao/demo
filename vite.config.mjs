3e9ebb4824509c8e75e4bde7267ed1abf90c0107
gaofeng9 <gaofeng9@kingsoft.com>
2024-08-30 11:13:03 +0800

    [sitemap生成]

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,14 @@
 import { fileURLToPath, URL } from "url";
-
+import Sitemap from 'vite-plugin-sitemap';
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Sitemap({ baseURL: 'www.example.com' }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
